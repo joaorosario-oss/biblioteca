f1f62de9d8f0d59817a7f98f52ddbfca94b2c083
joaorosario-oss <joao.rosario@edu.senai.br>
2026-04-28 07:59:09 -0300

    biblioteca

---
+++ biblioteca.php
@@ -0,0 +1,49 @@
+<?php
+
+function matematica;
+
+
+
+function somar($a,$b,$c){
+    return $a+$b+$c;
+}
+function subtrair ($a,$b){
+return $a-$b;
+
+}
+// commit v5
+echo "resultado: \n", subtrair (10,5);
+
+function multiplicar($a,$b){
+return $a*$b
+}
+
+echo "23/04/2026 eu aprendi a mexer no Github/Git  =D \n";
+
+    namespace texto{
+        function concatenar ($nome,$sobrenome)
+        return $nome . "" .$sobrenome;
+    }              
+namespace logica {
+functio verificaidade($idade){
+    if ($idade>=18){
+        echo "maior de idade!";
+    }
+else {
+    echo "menor de idade";
+}
+   function exercito ($sexo,$idade) {
+    if ($idade>=18 && $sexo == "masculino") {
+        return "Você está apto a servir ao exèrcito";
+
+    else {
+       return "Você não está apto para servir o exército";
+    }
+        }
+   } 
+
+}
+
+}
+
+<?
